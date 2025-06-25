8d43d7509a2219aa708debcbc68e5573e14528ea
kur0nek-o <maulana21mh@gmail.com>
2025-06-25 14:37:49 +0700

    fix: me endpoint

--- src/routes/meRoutes.js
+++ src/routes/meRoutes.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import authenticate from '../middlewares/authenticate.js'
+import User from '../models/User.js'
 
 const router = express.Router()
 
 router.get('/me', authenticate, async (req, res) => {
+  const userId = req.user.userId
+  const user = await User.findById(userId).select('username')
+
   res.json({
     status: true,
-    message: 'You are authenticated'
+    message: 'You are authenticated',
+    data: {
+      username: user.username
+    }
   })
 })
 
